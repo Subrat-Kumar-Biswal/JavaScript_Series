30a266eb5957d06dc9bfa03750adb6cff33c5aba
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-18 20:38:23 +0000

    Conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "chan"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+
+let valueInNumber =  Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)      //NaN - Not a Number
+
+
+//"33"  => 33 
+// "33ad" => NaN
+// ture => 1
+// false => 0
+
+let isLoggedIn = "chan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "chan"  => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
