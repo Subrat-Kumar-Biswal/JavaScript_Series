7bb070250ced1b2864a729cb811c73ceebd4854f
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-18 22:04:46 +0000

    conversion operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = "chan"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 
 let valueInNumber =  Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)      //NaN - Not a Number
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)      //NaN - Not a Number
 
 
 //"33"  => 33 
@@ -17,7 +17,7 @@ console.log(valueInNumber)      //NaN - Not a Number
 let isLoggedIn = "chan"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
@@ -27,5 +27,45 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+// ************************* Operations ******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " chandan"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2 );
+// console.log(1 + 2 + "2" + 8 );
+
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3  
+
+// num1 = num2 = num3 = 2+2
+
+console.table([num1,num2,num3]);
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
