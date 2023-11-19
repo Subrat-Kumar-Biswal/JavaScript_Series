d930cdb4011cef4679a17653fa6bdd5896169ada
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-19 17:27:16 +0000

    not complete

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,14 @@
+const name = "chandan"
+const repoCount = 1
+
+// console.log(name + repoCount + "value");
+
+//morden string interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);//string interpolation
+
+//declaration
+const gameName = new String ('chandan')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.charAt(5));
