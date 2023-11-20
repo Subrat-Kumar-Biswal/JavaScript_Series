bc1ac73e1886c0530e3922a3be45531cce9df009
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-20 13:17:38 +0000

    stirng method

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -10,5 +10,32 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);//stri
 const gameName = new String ('chandan')
 
 console.log(gameName[0]);
-console.log(gameName.__proto__);
+console.log(gameName.__proto__);   //{}
+
+
 console.log(gameName.charAt(5));
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));      
+console.log(gameName.indexOf('a'));
+
+
+console.log(gameName.slice(0,3));     // in slice we give -ve value
+console.log(gameName.slice(-3,-1));   
+
+console.log(gameName.substring(0,4));   // not give negative value 
+
+const newStringOne = "      subrat     "
+console.log(newStringOne)
+console.log(newStringOne.trim())     //(It work on white space or line terminate)remove space starting and ending
+console.log(newStringOne.trimStart())     //remove space starting space
+console.log(newStringOne.trimEnd())     //remove space at ending
+
+
+const url = "https://subrat.com"
+console.log(url.replace('subrat','chandan'))  //replace the value in the string
+
+console.log(url.includes('subrat'))   //return ture substring is present
+
+
+const str = 'The quick_brown fox_ jumps over _the lazy dog.';
+console.log(str.split('_'));
