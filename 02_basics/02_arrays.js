b4245f47413eac45f1178ab0c3ed24292069fa7a
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-22 17:53:45 +0000

    array2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,5 +1,9 @@
 const marvel_Heros = ["thor","spider","IronMan"]
 const dc_Heros = ["superman","batman","flash"]
+  
+// marvel_Heros.push(dc_Heros)
+// console.log(marvel_Heros);  //  [ 'thor', 'spider', 'IronMan', [ 'superman', 'batman', 'flash' ] ]
+// console.log(marvel_Heros[3]); 
 
-marvel_Heros.push(dc_Heros)
-console.log(dc_Heros);
+marvel_Heros.concat(dc_Heros)
+console.log(marvel_Heros); 
