3701b7cc17ad710be1502bbbc65e530e1bac9e10
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-26 10:53:04 +0000

    scope done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,12 +6,54 @@ if(true){
   let a = 10;
   const b = 30;
   var c = 20;
-  console.log("INNER: ",a);  //block scope
+  // console.log("INNER: ",a);  //block scope
 }
 
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
 
 
+// nested Scope
+
+function one(){
+  const username = "chandan"
+
+  function two (){
+    const website = "youtube"
+    console.log(username);
+  }
+  // console.log(website);  
+  // two()
+}
+
+one()
+
+if(true){
+  const username = "chandan"
+  if(username === "chandan"){
+    const website = " youtube"
+    console.log(username + website);
+  }
+  // console.log(website);  // return error website not defiened
+}
+
+// console.log(username);   //return error website not defiened
+
+
+// ++++++++++++++++++++++  interesting ++++++++++++
+
+
+console.log(addone(5))           // not return anything because  it just return not assign the variable
+function addone(num){
+  return num + 1
+}
+
+
+
+AddTwo(4)   // 'AddTwo' before initialization
+
+const AddTwo = function (num){         // it is experssion  different type to write function
+  return num  + 2
+}
