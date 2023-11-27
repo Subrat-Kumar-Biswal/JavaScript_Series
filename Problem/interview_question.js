c9becc9c8fcfca661062f13bae48061edbec4c28
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-27 12:10:25 +0000

    questions

---
+++ Problem/interview_question.js
@@ -0,0 +1,16 @@
+// let n = 2569;
+// count = 0
+// while(n!=0){
+//     let rem = n % 10;
+//     count = count + 1;
+//     n = n / 10;
+// }
+
+// console.log(count);
+
+// [1000343490884773].toString().split('').length
+
+
+console.log('age is ',age);
+var age = 52;
+console.log("aage si s",age);
