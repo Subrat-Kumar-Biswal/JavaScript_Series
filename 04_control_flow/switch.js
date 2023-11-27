7ea1bd5d833dcf4d8ae9bf34ebf8e0c8f3600a79
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-27 13:17:47 +0000

    Switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,52 @@
+// switch case
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+
+
+
+// switch (month) {
+//   case 1:
+//     console.log("january");
+//     break;
+//   case 2:
+//     console.log("Feb");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   default:
+//     console.log("default case match");
+// }
+
+// take string in switch case
+ 
+const month = "april"
+
+switch (month) {
+    case "jan":
+      console.log("1");
+      break;
+    case "feb":
+      console.log("2");
+      break;
+    case "march":
+      console.log("3");
+      break;
+    case "april":
+      console.log("4");
+      break;
+    default:
+      console.log("default case match");
+  }
