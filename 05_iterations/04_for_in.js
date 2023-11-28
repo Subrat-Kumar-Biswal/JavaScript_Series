2f1814631e858a01b728725ef331d0c480f35b4c
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-11-28 18:49:03 +0000

    for in loop

---
+++ 05_iterations/04_for_in.js
@@ -0,0 +1,20 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple',
+}
+
+// for in loop
+
+for (const key in myObject) {
+    // console.log(key);  // print key
+    // console.log(myObject[key]);  //print values
+    // console.log(`${key} shortcut is for: ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for(const key in programming){
+    console.log(key)
+}
