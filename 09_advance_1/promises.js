64ef371e1d8cd7db8d0161bc487d1e8236aabc91
Subrat Kumar Biswal <126277018+Subrat-Kumar-Biswal@users.noreply.github.com>
2023-12-05 18:04:52 +0000

    async promice awit

--- 09_advance_1/promises.js
+++ 09_advance_1/promises.js
@@ -39,12 +39,15 @@ new Promise(function(resolve, reject){
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
     resolve({username: "chandan", email: "chandan@gamil.com"})
-    }, 5000)
+    }, 2000)
 })
 
 promiseThree.then(function(user){
     console.log(user);
 })
+
+
+// promise Four
 
 const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
@@ -54,15 +57,65 @@ const promiseFour = new Promise(function(resolve, reject){
         }else{
             reject('ERROR: Something Went wrong')
         }
-    }, 2000)
+    }, 1000)
 })
 
 promiseFour.then((user) => {
     console.log(user);
     return user.username
-}).then((username) => {
+})
+.then((username) => {
     console.log(username);
-}).catch(function(error){
+})
+.catch(function(error){   // handle the error
     console.log(error);
 })
+.finally(() => console.log("the promise is either resolved or reject"))
 
+
+// promise five
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = true
+        if(!error){
+            resolve({username: "javascript", password: "1236"})
+        }else{
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000);
+})
+
+
+async function consumePromiseFive(){
+   try{
+    const response = await promiseFive
+   console.log(response);
+   }catch(error){
+    console.log(error);
+   }
+}
+consumePromiseFive()
+
+
+
+// async function  getAllUsers(){
+//   try{
+//     const response = await fetch('https://api.github.com/users/Subrat-Kumar-Biswal')
+//     const data = await response.json()
+//     console.log(data);
+//   }catch(error){
+//     console.log("E: ", error);
+//   }
+// }
+// getAllUsers()
+
+
+fetch('https://api.github.com/users/Subrat')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
